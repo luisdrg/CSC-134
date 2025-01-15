774c5d96ec85ad9e49682ce521b3ba655e0da899
Luis Rivera Gonzalez <riveragluisd@gmail.com>
2025-01-15 16:33:50 +0000

    Code hello world program

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+// CSC 134
+// Hello World
+// Mr. Norris
+// 1/15/25
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello, World!" << endl;
+   cout << "Codespaces is cool but I like to use VSCODE locally because I am used to it" << endl;
+   return 0;
+}
