be5bb86476b8fd2e95e222ec05d51028091779e9
Luis Rivera Gonzalez <riveragluisd@gmail.com>
2025-01-18 19:31:58 +0000

    Complete M1T2

---
+++ M1T2_RiveraLuis.cpp
@@ -0,0 +1,15 @@
+// CSC 134
+// M1T2
+// Luis Rivera Gonzalez
+// 1/18/25
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Name: Luis Rivera Gonzalez" << endl;
+   cout << "Enrolled in: CSC-134( C++), CSC-251 (Java), CTI-110 (Web/Python), NOS-125 (Linux Scripting)" << endl;
+   cout << "About me: I have two last names, but I usually got by Rivera. I like to play video games and learn about new technologies. My goal is to become a software engineer!" << endl;
+   return 0;
+}
