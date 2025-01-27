2858a031aec70200e81fe8437327e8d9ec12b8f4
Luis Rivera Gonzalez <riveragluisd@gmail.com>
2025-01-27 16:44:46 +0000

    Finish adding quotes

--- module1/M1HW1_RiveraGonzalez.cpp
+++ module1/M1HW1_RiveraGonzalez.cpp
@@ -14,4 +14,11 @@ double grossEarnings = 2.93;
 
 // print all the information about the movie
 cout << movieName << " was released in " << yearReleased << ", and had a worldwide gross earning of around " << grossEarnings << " billion dollars\n";
+
+//Quotes
+cout << "\nQuotes:\n";
+cout << "1. \"Yes, she is a good hunter. But I've already chosen. But this woman must also choose me.\" -Jake" << endl;
+cout << "2. \"Everything is backwards now, like out there is the true world, and in here is the dream.\" -Jake" << endl;
+cout << "3. \"This is our land!\" -Jake" << endl;
+cout << "4. \"You are not in Kansas anymore.\" -Quaritch" << endl;
 }
