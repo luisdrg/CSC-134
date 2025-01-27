7508cd412b17fc9c9294fc313406bdc40df464e5
Luis Rivera Gonzalez <riveragluisd@gmail.com>
2025-01-27 16:31:47 +0000

    Complete M1HW1

---
+++ module1/M1HW1_RiveraGonzalez.cpp
@@ -0,0 +1,17 @@
+// CSC 134
+// M1HW1
+// Luis Rivera Gonzalez
+// 1/27/25
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string movieName = "Avatar";
+int yearReleased = 2009;
+double grossEarnings = 2.93;
+
+// print all the information about the movie
+cout << movieName << " was released in " << yearReleased << ", and had a worldwide gross earning of around " << grossEarnings << " billion dollars\n";
+}
