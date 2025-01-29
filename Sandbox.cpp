e441d6235ae36415ef6932bcfac8bdada033794b
Luis Rivera Gonzalez <riveragluisd@gmail.com>
2025-01-29 15:57:55 +0000

    Place to test out code

---
+++ Sandbox.cpp
@@ -0,0 +1,14 @@
+//Place to test out code
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+//Apple sales program
+string user_name;
+cout << "Hello! What's your name? ";
+cin >> user_name;
+cout << "Thanks for coming by, " << user_name << "!" << endl;
+
+}
