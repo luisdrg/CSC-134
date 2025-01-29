9721b77d8e8885c74da2cbdbe4fec8240422d43f
Luis Rivera Gonzalez <riveragluisd@gmail.com>
2025-01-29 16:05:39 +0000

    Add setprecision method for 2 decimal places

--- module2/M2T1_RiveraGonzalez.cpp
+++ module2/M2T1_RiveraGonzalez.cpp
@@ -4,13 +4,14 @@
 // 1/29/25
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
 
 // Declare variables
 double mealPrice = 10.00;
-double taxPercent = 7.50;
+double taxPercent = 7.33;
 double taxAmount;
 double total;
 
@@ -19,7 +20,9 @@ cout << "Tax Percent: " << taxPercent << "%" <<endl;
 
 //Calculate Tax amount and Total
 taxAmount = mealPrice * (taxPercent / 100);
-cout << "Tax Amount: $" << taxAmount << endl;
+
+// fixed << setprecision(2) is used to make it have only 2 decimal places. #include <iomanip> must also be added to use the function
+cout << "Tax Amount: $" << fixed << setprecision(2) << taxAmount << endl;
 total = mealPrice + taxAmount;
 
 // Print the results
