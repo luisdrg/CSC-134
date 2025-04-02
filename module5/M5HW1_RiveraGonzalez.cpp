0e3b910a38e663ce733fe9a241029e6d9a893500
Luis Rivera Gonzalez <riveragluisd@gmail.com>
2025-04-02 15:40:07 +0000

    Complete Q3

--- module5/M5HW1_RiveraGonzalez.cpp
+++ module5/M5HW1_RiveraGonzalez.cpp
@@ -10,11 +10,13 @@ using namespace std;
 
 void question_1();
 void question_2();
+void question_3();
 
 int main() {
 
     //question_1();
-    question_2();
+    //question_2();
+    question_3();
 
 
     return 0;
@@ -47,4 +49,54 @@ void question_1() {
 // Q2
 void question_2() {
     cout << "-------------------------Question 2-------------------------" << endl;
+    double width;
+    while(true) {
+        cout << "Enter width: ";
+        cin >> width;
+        if(width > 0){
+            break;
+        }
+        cout << "Invalid value entered. Must be positive and greater than 0!" << endl;
+    }
+
+    double length;
+    while(true) {
+        cout << "Enter length: ";
+        cin >> length;
+        if(length > 0){
+            break;
+        }
+        cout << "Invalid value entered. Must be positive and greater than 0!" << endl;
+    }
+
+    double height;
+    while(true) {
+        cout << "Enter height: ";
+        cin >> height;
+        if(height > 0){
+            break;
+        }
+        cout << "Invalid value entered. Must be positive and greater than 0!" << endl;
+    }
+
+    double volume = width * length * height;
+    cout << "Volume: " << volume << endl;
+}
+
+// Q2
+void question_3() {
+    cout << "-------------------------Question 3-------------------------" << endl;
+    string roman_num[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+    cout << roman_num[1];
+
+    while(true){
+        cout << "Enter a number (1 - 10): ";
+        int input;
+        cin >> input;
+        if(input >= 1 && input <= 10){
+            cout << "The Roman numeral version of " << input << " is " << roman_num[input] << endl;
+            break;
+        }
+        cout << "Invalid! Number must be from 1 to 10.\n";
+    }
 }
